de106cdf100eee1e05a430f26e565e37d2bfcd10
Akinleyimu Gbotemi <gakinleyimu@gmail.com>
2025-04-14 14:52:38 +0100

    implement homepage redirect

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -81,3 +81,32 @@ try {
 }
     
 }
+
+
+export async function getCurrentUser(): Promise<User | null>{
+    const cookieStore = await cookies();
+    const sessionCookie = cookieStore.get('session')?.value;
+
+    if(!sessionCookie) return null
+    try {
+            const decodedClaims = await auth.verifySessionCookie(sessionCookie, true)
+
+            const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+
+            if(!userRecord.exists) return null;
+
+            return{
+                ...userRecord.data(),
+                id: userRecord.id,
+            } as User;
+        
+        } catch (e) {
+        console.log(e)
+    }
+}
+
+export async function isAuthenticated(){
+    const user = await getCurrentUser();
+
+    return !!user; 
+}
